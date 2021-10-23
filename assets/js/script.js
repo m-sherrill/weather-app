5f8b5a21a0d97a897886497b5ce99be4696927c5
Morgan Sherrill <mo.sherrill@outlook.com>
2021-10-22 23:26:09 -0700

    overroad some materialize styling and worked on other styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ function fetchComplete() {
 
             // Current Stats
             currentTemp = Math.round((data.current.temp))
-            currentHumidity = (data.current.humidity)
+            currentHumidity = Math.round((data.current.humidity))
             currentIcon = (data.current.weather[0].icon)
             currentWindSpeed = Math.round((data.current.wind_speed))
             currentUVI = (data.current.uvi)
@@ -79,9 +79,9 @@ function fetchComplete() {
             // Future Data Loop
             for (let i = 0; i < futureArray.length; i++) {
                 futureIcon = (data.daily[i].weather[0].icon)
-                futureTemp = (data.daily[i].temp.day)
-                futureHumidity = (data.daily[i].humidity)
-                futureWindSpeed = (data.daily[i].wind_speed)
+                futureTemp = Math.round((data.daily[i].temp.day))
+                futureHumidity = Math.round((data.daily[i].humidity))
+                futureWindSpeed = Math.round((data.daily[i].wind_speed))
                 futureIconArray.push(futureIcon)
                 futureTempArray.push(futureTemp)
                 futureHumidityArray.push(futureHumidity)
@@ -91,12 +91,12 @@ function fetchComplete() {
 
             // Future Day Loop
             for (let i = 1; i < futureArray.length; i++) {
-                futureDay = moment().add([i], 'days').format("MMM Do YY")
+                futureDay = moment().add([i], 'days').format("MMM Do YYYY")
                 futureDayArray.push(futureDay)
-                
+
 
             }
-            
+
             addContent()
         })
 }
@@ -108,7 +108,7 @@ function addContent() {
     let currentHTML = `<div class="row" id="current-day-header">What can I expect today? Current Forecast</div><div class="container center-align">
     <div class="row" id="current-city">${currentName}<h6>${currentDay}<h6></div>
     <div class="container" id="current-flex">
-        <div id="current-el"><h6>Tempurature</h6><p>${currentTemp}° F</p></div>
+        <div id="current-el"><h6>Temperature</h6><p>${currentTemp}° F</p></div>
         <div id="current-icon"><img src="https://openweathermap.org/img/wn/${currentIcon}@2x.png"></div>
         <div id="current-el"><h6>Humidity</h6><p>${currentHumidity}%</p></div>
     </div>
@@ -123,7 +123,7 @@ function addContent() {
 
     for (let i = 0; i < futureArray.length - 1; i++) {
 
-        let fiveDayHTMLContent = `<div class="" id="five-day-el" name="day"><h6>${futureDayArray[i]}</h6><img src="https://openweathermap.org/img/wn/${futureIconArray[i]}@2x.png"><br/>Tempurature: ${futureTempArray[i]}°F<br/>Humidity: ${futureHumidityArray[i]}%<br/>Wind Speed: ${futureWindSpeedArray[i]}mph</div>`
+        let fiveDayHTMLContent = `<div class="" id="five-day-el" name="day"><h6>${futureDayArray[i]}</h6><img src="https://openweathermap.org/img/wn/${futureIconArray[i]}@2x.png"><br/><b>Temperature:</b> ${futureTempArray[i]}°F<br/><b>Humidity:</b> ${futureHumidityArray[i]}%<br/><b>Wind Speed:</b> ${futureWindSpeedArray[i]} mph</div>`
         $("#five-day-container").append(fiveDayHTMLContent)
 
     }
@@ -136,9 +136,10 @@ function newSearch() {
 
 // Taking the data from user input
 searchBtnEl.on("click", function () {
-    
+
     cityURL = `https://api.openweathermap.org/data/2.5/weather?q=${city.val()}&units=imperial&appid=${apiKey}`
 
+    $("#five-day-header").css("display", "block")
     //adding Search Item to Local Storage
     localStorageArray.push(city.val())
     localStorage.setItem("City", JSON.stringify(localStorageArray))
@@ -147,10 +148,11 @@ searchBtnEl.on("click", function () {
     let pTag = $("<p>")
     let savedBtn = $("<button>")
     savedBtn.attr("id", "saved-btn")
+    savedBtn.attr("class", "btn waves-effect waves indigo")
     savedBtn.attr("name", city.val())
     savedBtn.html(city.val())
     pTag.append(savedBtn)
-    $("#local-storage-history").append(pTag)
+    $("#local-storage-history").prepend(pTag)
 
 
     //Starting the first fetch
@@ -162,19 +164,20 @@ searchBtnEl.on("click", function () {
 // populates the page on load with what it is in local storage
 function localStorageSave() {
     let savedCities = JSON.parse(localStorage.getItem("City")) || []
-    
+
 
     for (let i = 0; i < savedCities.length; i++) {
-        
-            let pTag = $("<p>")
-            let savedBtn = $("<button>")
-            savedBtn.attr("id", "saved-btn")
-            savedBtn.attr("name", savedCities[i])
-            savedBtn.html(savedCities[i])
-            pTag.append(savedBtn)
-            $("#local-storage-history").append(pTag)
 
-        }
+        let pTag = $("<p>")
+        let savedBtn = $("<button>")
+        savedBtn.attr("id", "saved-btn")
+        savedBtn.attr("class", "btn waves-effect waves-white indigo")
+        savedBtn.attr("name", savedCities[i])
+        savedBtn.html(savedCities[i])
+        pTag.append(savedBtn)
+        $("#local-storage-history").prepend(pTag)
+
+    }
 }
 
 // 
@@ -182,7 +185,20 @@ function localStorageSave() {
 localStorageSave()
 
 
-$(document).on("click", "#saved-btn", function() {
+$(document).on("click", "#saved-btn", function () {
     cityURL = `https://api.openweathermap.org/data/2.5/weather?q=${$(this).attr("name")}&units=imperial&appid=${apiKey}`
+    $("#five-day-header").css("display", "block")
     fetchUserQuery(cityURL)
-} )
+})
+
+$("#clear-storage-btn").on("click", function () {
+    $("#local-storage-history").text("")
+    localStorage.removeItem("City")
+    
+}
+)
+
+
+$(document).ready(function(){
+    $('.carousel').carousel()
+  })
